de5330bc43bc4458cde72f7ea8aaa9d6a019ad44
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-01-14 20:34:24 +0530

    footer url added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -87,7 +87,7 @@ export default function Footer() {
 
       <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2020 Copyright:
-        <a className='text-dark' href='https://mdbootstrap.com/'>
+        <a className='text-dark' href='https://rushikesh-sapkal-portfolio.vercel.app'>
           Made with ❤️ by Rushikesh
         </a>
       </div>

f491c4fdfa1535ca387d4de80460d9433c17568d
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-01-14 20:30:47 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './index.css'
 import './Components/Hero.css'
 import Hero from './Components/Hero'
 import Home from './Components/Home'
+import Footer from './Components/Footer'
 const App = () => {
   return (
     <div>
        <Hero/>
        <Home/>
+       <Footer/>
 
     </div>
   )
